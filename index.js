614c905c33b03380483454ac3ecc6124d06fe9d0
programmermame <mameatpickout@gmail.com>
2025-06-25 18:09:48 +0300

    Add timestamp micorservice

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,9 +21,45 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api", (req, res) => {
+  try {
+    let currentDate = new Date();
+    let utc = currentDate.toUTCString();
+    let unix = currentDate.getTime();
+    res.json({
+      "unix": unix,
+      "utc": utc
+    })
+
+  } catch (error) {
+    res.json({ "Error": error.message })
+  }
+})
+
+app.get("/api/:date", (req, res) => {
+  try {
+    let date = isNaN(req.params.date) ? new Date(req.params.date) : new Date(Number(req.params.date));
+
+
+    if (isNaN(date.getTime())) {
+      res.json({ error: "Invalid Date" })
+    }
+
+    let utc = date.toUTCString();
+    let unix = date.getTime();
+
+    res.json({
+      "unix": unix,
+      "utc": utc
+    })
+
+  } catch (error) {
+    res.json({ "Error": error.message })
+  }
+})
 
 
 // Listen on port set in environment variable or default to 3000
